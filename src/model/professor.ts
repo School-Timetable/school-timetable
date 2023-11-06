2b95fd98a794cfdc1ad7fce58ba51527a9c38510
Francesco Gallo <francescogallo728@gmail.com>
2023-11-06 18:24:59 +0100

    zod dependency and model scratch

---
+++ src/model/professor.ts
@@ -0,0 +1,14 @@
+export class Professor {
+    constructor(name: string, surname: string, email: string, subjects: []) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.subjects = subjects;
+    }
+    
+    id: number = 0;
+    name: string;
+    surname: string;
+    email: string;
+    subjects: [];
+}
