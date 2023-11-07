88fb6625fae042da6b0e546add85d49615283376
Lebonsky <gionnym@icloud.com>
2023-11-07 13:34:18 +0100

    changes on .gitignore and added .svelte-kit folder for building

---
+++ .svelte-kit/output/server/manifest-full.js
@@ -0,0 +1,34 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.189df7e9.js","app":"_app/immutable/entry/app.52631a31.js","imports":["_app/immutable/entry/start.189df7e9.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/singletons.b50249e2.js","_app/immutable/entry/app.52631a31.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.a21d6cee.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js')),
+			__memo(() => import('./nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+}
+})();
