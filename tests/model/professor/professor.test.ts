b894923f0f1632c052fe3493f944010f77943358
Francesco Gallo <francescogallo728@gmail.com>
2023-11-08 13:22:05 +0100

    const value for professor test

--- tests/model/professor/professor.test.ts
+++ tests/model/professor/professor.test.ts
@@ -5,12 +5,14 @@ import { Cellphone } from "../../../src/model/professor/cellphone";
 import { Mail } from "../../../src/model/professor/mail";
 import { test, expect } from "@jest/globals";
 
-test('professor_entity_test', () => {
-    const name = new Name("John");
-    const surname = new Surname("Claire");
-    const cellphone = new Cellphone("123456789");
-    const mail = new Mail("abcd13@gmail.com");
-    const professor = new Professor(name, surname, mail, cellphone);
+const name = new Name("John");
+const surname = new Surname("Claire");
+const cellphone = new Cellphone("123456789");
+const mail = new Mail("abcd13@gmail.com");
+const professor = new Professor(name, surname, mail, cellphone);
+
+
+test('professor_entity_fixture_test', () => {
     expect(professor.name.value).toBe(name.value);
     expect(professor.surname.value).toBe(surname.value);
     expect(professor.cellPhone.value).toBe(cellphone.value);
@@ -18,21 +20,21 @@ test('professor_entity_test', () => {
 });
 
 test('professor_entity_test_name_change', () => {
-    const name = new Name("John");
-    const surname = new Surname("Claire");
-    const cellphone = new Cellphone("123456789");
-    const mail = new Mail("abcd12@gmail.com");
-    const professor = new Professor(name, surname, mail, cellphone);
     professor.name = new Name("Johnny");
     expect(professor.name.value).toBe("JOHNNY");
 });
 
 test('professor_entity_test_surname_change', () => {
-    const name = new Name("John");
-    const surname = new Surname("Claire");
-    const cellphone = new Cellphone("123456789");
-    const mail = new Mail("abcd12@gmail.com");
-    const professor = new Professor(name, surname, mail, cellphone);
     professor.surname = new Surname("Hill");
     expect(professor.surname.value).toBe("HILL");
+});
+
+test('professor_entity_test_cellphone_change', () => {
+    professor.cellPhone = new Cellphone("987654321");
+    expect(professor.cellPhone.value).toBe("987654321");
+});
+
+test('professor_entity_test_mail_change', () => {
+    professor.email = new Mail("asdfg@hotmail.com");
+    expect(professor.email.value).toBe("asdfg@hotmail.com");
 });
