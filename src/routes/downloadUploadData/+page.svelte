bc98b86e32496e304a62b3540dd5a34951a6f08f
gabtass9 <gabrieletassone9@gmail.com>
2023-11-18 23:15:58 +0100

    added new folder, containing first sprint of user stories about download and upload files

---
+++ src/routes/downloadUploadData/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import DownloadComponent from './DownloadComponent.svelte';
+    import UploadComponent from './UploadComponent.svelte';
+  
+  </script>
+  
+  <main>
+
+<h1>DOWNLOAD COMPONENT</h1>
+    <DownloadComponent/>
+    <hr>
+    <h1>UPLOAD COMPONENT</h1>
+    <UploadComponent/>
+  </main>
+  
