ade1a568619953b34678e7566a668f341cb38436
Francesco Gallo <francescogallo728@gmail.com>
2023-11-19 21:37:08 +0100

    moved in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,9 @@
 	import { generateCookieFile } from "$lib/utils/cookie_file_writer";
 	import { onMount } from "svelte";
 	import { Container, Styles } from "sveltestrap";
+	import { goto } from "$app/navigation";
+	import ThemeSwitcher from "$lib/components/ThemeSwitcher.svelte";
+    import { Navbar, NavbarBrand } from "sveltestrap";
 
 	onMount(() => {
 		const generate_file = () =>
@@ -22,8 +25,48 @@
 	});
 </script>
 
+
 <Styles theme={$theme} />
 
+<Navbar expand="md">
+	<div class="left-group">
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+		<p id="title" class="zoom-hover" on:click={() => {goto("/")}} >SCHOOL TIMETABLE</p>
+		
+		<div class="vl"></div>
+		<div class="zoom-hover">
+			<NavbarBrand style="margin-left: 20px;" href="/data-input-form"> Input Form </NavbarBrand>
+		</div>
+		
+	</div>
+
+	<ThemeSwitcher />
+
+</Navbar>
 <Container xxl class="bg-body rounded shadow mt-2">
 	<slot />
 </Container>
+
+
+<style>
+    p {
+        font-size: 23px;
+        font-weight: bold;
+        cursor: pointer;
+        margin: 0 0 0 30px;
+        padding: 0;
+    }
+
+    .left-group {
+        display: flex;
+        align-items: center;
+    }
+
+    .vl {
+        border-left: 1px solid white;
+        height: 30px;
+        margin-left: 20px;
+    }
+        
+</style>
