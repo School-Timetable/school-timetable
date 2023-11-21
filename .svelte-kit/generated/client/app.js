aa8c2c9d4629b3afbc702fcec63ffb4dab60f220
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-11-21 18:06:18 +0100

    merged both group main branches

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -5,15 +5,17 @@ export const nodes = [
 	() => import('./nodes/1'),
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
-	() => import('./nodes/4')
+	() => import('./nodes/4'),
+	() => import('./nodes/5')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
 		"/": [2],
-		"/downloadUploadData": [3],
-		"/drag-and-drop": [4]
+		"/data-input-form": [3],
+		"/downloadUploadData": [4],
+		"/drag-and-drop": [5]
 	};
 
 export const hooks = {
