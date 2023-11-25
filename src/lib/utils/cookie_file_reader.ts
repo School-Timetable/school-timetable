ec887df448693b6379b1ae6d6336a85532840da4
Simone Ventrici <simoven.dev@gmail.com>
2023-11-25 18:44:17 +0100

    Added localStorage support for hourOfDay and dayOfweek

--- src/lib/utils/cookie_file_reader.ts
+++ src/lib/utils/cookie_file_reader.ts
@@ -1,6 +1,8 @@
 import { Professor } from "$model/professor/professor";
 import { SchoolClass } from "$model/school-class/school-class";
 import { Subject } from "$model/subject/subject";
+import { DayOfWeek } from "$model/timetable/day-of-week";
+import { HourOfDay } from "$model/timetable/hour-of-day";
 
 export function getExistingProfessorFromFile(file_data: string[]) {
     let professors: Professor[] = []
@@ -36,3 +38,25 @@ export function getExistingSubjectsFromFile(file_data: string[], existing_prof: 
     return subjects;
 }
 
+export function getExistingHoursOfDayFromFile(file_data: string[]) {
+    let hoursOfDay: HourOfDay[] = [];
+    file_data.forEach(line => {
+        if (line.substring(0, 2) === "H:") {
+            hoursOfDay.push(HourOfDay.ofCsv(line))
+        }
+    })
+
+    return hoursOfDay;
+}
+
+
+export function getExistingDaysOfWeekFromFile(file_data: string[]) {
+    let daysOfWeek: DayOfWeek[] = [];
+    file_data.forEach(line => {
+        if (line.substring(0, 2) === "D:") {
+            daysOfWeek.push(DayOfWeek.ofCsv(line))
+        }
+    })
+
+    return daysOfWeek;
+}
--- src/lib/utils/cookie_file_writer.ts
+++ src/lib/utils/cookie_file_writer.ts
@@ -1,8 +1,17 @@
 import type { Professor } from "$model/professor/professor";
 import type { SchoolClass } from "$model/school-class/school-class";
 import type { Subject } from "$model/subject/subject";
+import type { DayOfWeek } from "$model/timetable/day-of-week";
+import type { HourOfDay } from "$model/timetable/hour-of-day";
 
-export function generateCookieFile(all_professor: Professor[], all_classrooms: SchoolClass[], all_subjects: Subject[], base64: boolean = true) {
+export function generateCookieFile(
+    all_professor: Professor[], 
+    all_classrooms: SchoolClass[], 
+    all_subjects: Subject[], 
+    all_hoursOfDay: HourOfDay[],
+    all_daysOfWeek: DayOfWeek[],
+    base64: boolean = true
+) {
     let global_lines: string[] = [];
 
     all_professor.map((e) => e.toCsv()).forEach(element => {
@@ -16,6 +25,14 @@ export function generateCookieFile(all_professor: Professor[], all_classrooms: S
     all_subjects.map((e) => e.toCsv()).forEach(element => {
         global_lines.push(element);
     });
+
+    all_hoursOfDay.map((e) => e.toCsv()).forEach(element => {
+        global_lines.push(element);
+    })
+
+    all_daysOfWeek.map((e) => e.toCsv()).forEach(element => {
+        global_lines.push(element);
+    })
 
     const file_str = global_lines.join("\n");
 
