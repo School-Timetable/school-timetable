8cce903664fc57075c0641d5d1d458cd02bc38ed
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-11-25 17:15:39 +0100

    more tests for time table

--- src/model/TimeTable.ts
+++ src/model/TimeTable.ts
@@ -1,3 +1,4 @@
+import { log } from "console";
 import { Professor } from "./professor/professor";
 import { SchoolClass } from "./school-class/school-class";
 import { Subject } from "./subject/subject";
@@ -9,6 +10,18 @@ const _professorTimetableMap: Map<string, TimeTable> = new Map();
 
 export const classTimetableMap: ReadonlyMap<string, TimeTable> = _classTimetableMap;
 export const professorTimetableMap: ReadonlyMap<string, TimeTable> = _professorTimetableMap;
+
+
+// TODO: remove
+const daysPerWeek = 6;
+const hoursPerDay = 8;
+
+const mockClass = SchoolClass.of("class-mock", 1, "A");
+const mockProfessor = Professor.of("professor-mock", "mock", "mock", "fake@ema.il", "0000000000");
+
+// TODO: Create a more elegant solution
+export const unavailableSubject = Subject.of("unavailable", mockClass, mockProfessor, "unavailable", "X", 1, 10);
+
 
 class TimeTable {
 
@@ -37,7 +50,7 @@ class TimeTable {
         return this.values[dayOfWeek][timeOfDay];
     }
 
-    setSubjectOn(subject: Subject | null, dayOfWeek: number, timeOfDay: number): void {
+    setSubjectOn(dayOfWeek: number, timeOfDay: number, subject: Subject | null): void {
         if (!this.isValidTimeslot(dayOfWeek, timeOfDay)) {
             throw new Error("Invalid timeslot");
         }
@@ -45,51 +58,64 @@ class TimeTable {
         this.values[dayOfWeek][timeOfDay] = subject;
     }
 
+    /**
+     * @returns true if and only if the timeslot is not marked as unavailable
+     */
     isAvailableOn(dayOfWeek: number, timeOfDay: number): boolean {
         return this.getSubjectOn(dayOfWeek, timeOfDay) != unavailableSubject;
     }
 
+    /**
+     * @returns true if and only if the timeslot is marked as unavailable
+     */
     isUnavailableOn(dayOfWeek: number, timeOfDay: number): boolean {
         return this.getSubjectOn(dayOfWeek, timeOfDay) == unavailableSubject;
     }
 
+    /**
+     * @returns true if and only if the timeslot is assigned to a subject (not null and not unavailable)
+     */
     isAssignedOn(dayOfWeek: number, timeOfDay: number): boolean {
         const sub = this.getSubjectOn(dayOfWeek, timeOfDay);
         return sub != null && sub != unavailableSubject;
     }
 
+
+    /**
+     * ensures that a subject can be assigned the timeslot
+     * @returns true if the timeslot is not assigned to any subject
+     */
     isUnassignedOn(dayOfWeek: number, timeOfDay: number): boolean {
         return this.getSubjectOn(dayOfWeek, timeOfDay) == null;
     }
 
     isValidTimeslot(dayOfWeek: number, timeOfDay: number): boolean {
         return 0 <= dayOfWeek && dayOfWeek < this.daysPerWeek &&
-            0 <= timeOfDay && timeOfDay < this.hoursPerDay;
+            0 <= timeOfDay && timeOfDay < this.hoursPerDay &&
+            Number.isInteger(dayOfWeek) && Number.isInteger(timeOfDay);
     }
 
-
-
+    clear(): void {
+        for (let i = 0; i < this.daysPerWeek; i++) {
+            for (let j = 0; j < this.hoursPerDay; j++) {
+                this.values[i][j] = null;
+            }
+        }
+    }
 }
 
-// TODO: remove
-const daysPerWeek = 6;
-const hoursPerDay = 8;
 
-const mockClass = SchoolClass.of("class-mock", 1, "A");
-const mockProfessor = Professor.of("professor-mock", "mock", "mock", "fake@ema.il", "0000000000");
 
-const unavailableSubject = Subject.of("unavailable", mockClass, mockProfessor, "unavailable", "X", 1, 10);
-
-export function setSubject(subject: Subject, dayOfWeek: number, timeOfDay: number): void {
+export function setSubject(dayOfWeek: number, timeOfDay: number, subject: Subject): void {
 
     const classTimeTable = getClassTimetableOf(subject.schoolClass);
-    setSubjectOnTimeTable(subject, classTimeTable, dayOfWeek, timeOfDay);
+    setSubjectOnTimeTable(dayOfWeek, timeOfDay, subject, classTimeTable);
 
     const professorTimeTable = getProfessorTimetableOf(subject.professor);
-    setSubjectOnTimeTable(subject, professorTimeTable, dayOfWeek, timeOfDay);
+    setSubjectOnTimeTable(dayOfWeek, timeOfDay, subject, professorTimeTable);
 }
 
-function setSubjectOnTimeTable(subject: Subject, timeTable: TimeTable, dayOfWeek: number, timeOfDay: number) {
+function setSubjectOnTimeTable(dayOfWeek: number, timeOfDay: number, subject: Subject, timeTable: TimeTable) {
     const oldSubject = timeTable.getSubjectOn(dayOfWeek, timeOfDay);
 
     if (oldSubject == unavailableSubject) {
@@ -97,34 +123,53 @@ function setSubjectOnTimeTable(subject: Subject, timeTable: TimeTable, dayOfWeek
     }
     else if (oldSubject != null) {
         // we replace the old subject from all the timetables
-        removeSubject(oldSubject, dayOfWeek, timeOfDay);
+        removeSubject(dayOfWeek, timeOfDay, oldSubject);
     }
 
-    timeTable.setSubjectOn(subject, dayOfWeek, timeOfDay);
+    timeTable.setSubjectOn(dayOfWeek, timeOfDay, subject);
 }
 
-export function removeSubject(subject: Subject, dayOfWeek: number, timeOfDay: number) {
-    getClassTimetableOf(subject.schoolClass).setSubjectOn(null, dayOfWeek, timeOfDay);
-    getProfessorTimetableOf(subject.professor).setSubjectOn(null, dayOfWeek, timeOfDay);
+export function removeSubject(dayOfWeek: number, timeOfDay: number, subject: Subject) {
+    if (subject == unavailableSubject) {
+        throw new Error("Trying to remove an unavailable subject");
+    }
 
+    const ctt = getClassTimetableOf(subject.schoolClass);
+    const ptt = getProfessorTimetableOf(subject.professor);
+
+    if (ctt.getSubjectOn(dayOfWeek, timeOfDay) != subject || ptt.getSubjectOn(dayOfWeek, timeOfDay) != subject) {
+        throw new Error("Trying to remove a subject from a timeslot that doesn't contain it");
+    }
+
+    ctt.setSubjectOn(dayOfWeek, timeOfDay, null);
+    ptt.setSubjectOn(dayOfWeek, timeOfDay, null);
 }
 
 
-
-export function setUnavailable(dayOfWeek: number, timeOfDay: number, entity: SchoolClass | Professor, remove: boolean = false) {
+export function setUnavailable(dayOfWeek: number, timeOfDay: number, entity: SchoolClass | Professor, available: boolean = false) {
     const timeTable = getTimetableOf(entity);
     const oldSubject = timeTable.getSubjectOn(dayOfWeek, timeOfDay);
 
-    if (timeTable.isAssignedOn(dayOfWeek, timeOfDay)) {
-        throw new Error("Trying to setUnavailable on an assigned timeslot");
+
+
+    if (available) {
+        if (oldSubject != unavailableSubject) {
+            // throw new Error("Trying to remove a subject from an available timeslot");
+            return; // nothing to do
+        }
+    }
+    else {
+        if (timeTable.isAssignedOn(dayOfWeek, timeOfDay)) {
+            throw new Error("Trying to set unavailable on an assigned timeslot");
+        }
     }
 
-    if (remove && oldSubject != unavailableSubject) {
-        throw new Error("Trying to remove a subject from an available timeslot");
-    }
+    const newSubject = available ? null : unavailableSubject;
+    timeTable.setSubjectOn(dayOfWeek, timeOfDay, newSubject);
+}
 
-    const newSubject = remove ? null : unavailableSubject;
-    timeTable.setSubjectOn(newSubject, dayOfWeek, timeOfDay);
+export function setAvailable(dayOfWeek: number, timeOfDay: number, entity: SchoolClass | Professor) {
+    setUnavailable(dayOfWeek, timeOfDay, entity, true);
 }
 
 export function getClassTimetableOf(schoolClass: SchoolClass): TimeTable {
@@ -153,8 +198,14 @@ export function getTimetableOf(entity: SchoolClass | Professor): TimeTable {
         return getProfessorTimetableOf(entity);
     }
     else {
-        throw new Error("what is not a SchoolClass or a Professor");
+        throw new Error("entity is not a SchoolClass or a Professor");
     }
+}
+
+
+export function clearAll(): void {
+    _classTimetableMap.clear();
+    _professorTimetableMap.clear();
 }
 
 
@@ -163,5 +214,3 @@ export function getTimetableOf(entity: SchoolClass | Professor): TimeTable {
 // }
 
 
-
-// TODO: TEST THIS SHIT
--- tests/model/time-table.test.ts
+++ tests/model/time-table.test.ts
@@ -1,8 +1,7 @@
-import { classTimetableMap, professorTimetableMap, setSubject, removeSubject } from "$model/TimeTable";
+import { classTimetableMap, professorTimetableMap, setSubject, getClassTimetableOf, getProfessorTimetableOf, getTimetableOf, removeSubject, setUnavailable, setAvailable, clearAll } from "$model/TimeTable";
 import { Professor } from "$model/professor/professor";
 import { SchoolClass } from "$model/school-class/school-class";
 import { Subject } from "$model/subject/subject";
-import { set } from "zod";
 
 
 const professors = [
@@ -15,7 +14,7 @@ const classes = [
     SchoolClass.of("class-2", 2, "A")
 ]
 
-let subjects = [
+const subjects = [
     Subject.of("subj-1", classes[0], professors[0], "Matematica", "MAT", 5, 20),
     Subject.of("subj-2", classes[0], professors[1], "Italiano", "ITA", 6, 18),
     Subject.of("subj-3", classes[1], professors[0], "Fisica quantistica per bambini", "FIS", 5, 20),
@@ -24,23 +23,165 @@ let subjects = [
 
 describe("Timetable", () => {
 
-    it("should update readonly maps when timetable changes", () => {
-
-
-        setSubject(subjects[0], 0, 0);
-        expect(classTimetableMap.get("class-1")!.values[0][0]).toBe(subjects[0]);
-        expect(professorTimetableMap.get("prof-1")!.values[0][0]).toBe(subjects[0]);
-        setSubject(subjects[3], 0, 0);
-        expect(classTimetableMap.get("class-2")!.values[0][0]).toBe(subjects[3]);
-        expect(professorTimetableMap.get("prof-2")!.values[0][0]).toBe(subjects[3]);
-
-        expect(classTimetableMap.get("class-1")!.values[0][0]).toBe(subjects[0]);
-        expect(professorTimetableMap.get("prof-1")!.values[0][0]).toBe(subjects[0]);
-
-        setSubject(subjects[1], 0, 0);
-        expect(classTimetableMap.get("class-1")!.values[0][0]).toBe(subjects[1]);
-        expect(professorTimetableMap.get("prof-2")!.values[0][0]).toBe(subjects[1]);
-
-        expect(professorTimetableMap.get("prof-1")!.values[0][0]).toBe(null);
+    beforeEach(() => {
+        clearAll();
     });
+
+    it("Time table should be empty at the beginning", () => {
+        expect(classTimetableMap.size).toBe(0);
+        expect(professorTimetableMap.size).toBe(0);
+    });
+
+    it("Time table should create on demand", () => {
+        const c0tt = getClassTimetableOf(classes[0])
+
+        expect(c0tt).not.toBeNull();
+        expect(c0tt.daysPerWeek).toBeGreaterThan(0);
+        expect(c0tt.hoursPerDay).toBeGreaterThan(0);
+        expect(c0tt.values.length).toEqual(c0tt.daysPerWeek);
+        for (let i = 0; i < c0tt.daysPerWeek; i++) {
+            expect(c0tt.values[i].length).toEqual(c0tt.hoursPerDay);
+        }
+        expect(classTimetableMap.size).toBe(1);
+
+        const p0tt = getProfessorTimetableOf(professors[0])
+        expect(p0tt).not.toBeNull();
+        expect(p0tt.daysPerWeek).toBeGreaterThan(0);
+        expect(p0tt.hoursPerDay).toBeGreaterThan(0);
+        expect(p0tt.values.length).toEqual(p0tt.daysPerWeek);
+        for (let i = 0; i < p0tt.daysPerWeek; i++) {
+            expect(p0tt.values[i].length).toEqual(p0tt.hoursPerDay);
+        }
+        expect(professorTimetableMap.size).toBe(1);
+
+    });
+
+
+    it("Time table should handle overlaps by removing old subjects", () => {
+
+        const c0tt = getTimetableOf(classes[0]);
+        const p0tt = getTimetableOf(professors[0]);
+        const c1tt = getTimetableOf(classes[1]);
+        const p1tt = getTimetableOf(professors[1]);
+
+
+
+        setSubject(0, 0, subjects[0]);
+        expect(c0tt.getSubjectOn(0, 0)).toBe(subjects[0]);
+        expect(p0tt.getSubjectOn(0, 0)).toBe(subjects[0]);
+
+        setSubject(0, 0, subjects[3]);
+        expect(c1tt.getSubjectOn(0, 0)).toBe(subjects[3]);
+        expect(p1tt.getSubjectOn(0, 0)).toBe(subjects[3]);
+
+        // subject 0 didn't get removed from subject 0 and class 0
+        // because it was added to class 1 and professor 1, so there is no overlap
+        expect(c0tt.getSubjectOn(0, 0)).toBe(subjects[0]);
+        expect(p0tt.getSubjectOn(0, 0)).toBe(subjects[0]);
+
+        // subject 1 has the same class as subject 0, so there is an overlap which removes subject 0
+        // from class 0 and professor 0
+        // there's also an overlap with subject 3, which removes subject 3 from class 1 and professor 1
+        // and sets subject 1 to class 0 and professor 1
+        setSubject(0, 0, subjects[1]);
+
+        expect(c0tt.getSubjectOn(0, 0)).toBe(subjects[1]);
+        expect(p1tt.getSubjectOn(0, 0)).toBe(subjects[1]);
+
+        expect(c1tt.getSubjectOn(0, 0)).toBeNull();
+        expect(p0tt.getSubjectOn(0, 0)).toBeNull();
+
+    });
+
+    it("should remove subjects", () => {
+        const c0tt = getTimetableOf(classes[0]);
+        const p0tt = getTimetableOf(professors[0]);
+
+        setSubject(0, 0, subjects[0]);
+
+        expect(c0tt.getSubjectOn(0, 0)).toBe(subjects[0]);
+        expect(p0tt.getSubjectOn(0, 0)).toBe(subjects[0]);
+
+        removeSubject(0, 0, subjects[0]);
+
+        expect(c0tt.getSubjectOn(0, 0)).toBeNull();
+        expect(p0tt.getSubjectOn(0, 0)).toBeNull();
+    });
+
+    it("should set unavailable", () => {
+        const c0tt = getTimetableOf(classes[0]);
+        const p0tt = getTimetableOf(professors[0]);
+
+
+        expect(c0tt.isAvailableOn(0, 0)).toBe(true);
+        expect(p0tt.isAvailableOn(0, 0)).toBe(true);
+
+        setUnavailable(0, 0, classes[0]);
+
+        // the unvaliability is set only on the class timetable
+        expect(c0tt.isAvailableOn(0, 0)).toBe(false);
+        expect(p0tt.isAvailableOn(0, 0)).toBe(true);
+
+        setUnavailable(0, 0, professors[0]);
+
+        expect(c0tt.isAvailableOn(0, 0)).toBe(false);
+        expect(p0tt.isAvailableOn(0, 0)).toBe(false);
+
+        setAvailable(0, 0, professors[0]);
+
+        expect(c0tt.isAvailableOn(0, 0)).toBe(false);
+        expect(p0tt.isAvailableOn(0, 0)).toBe(true);
+
+        setAvailable(0, 0, classes[0]);
+
+        expect(c0tt.isAvailableOn(0, 0)).toBe(true);
+        expect(p0tt.isAvailableOn(0, 0)).toBe(true);
+    });
+
+    it("should throw when setting unavailable on an assigned timeslot", () => {
+        setSubject(0, 0, subjects[0]);
+
+        expect(() => setUnavailable(0, 0, classes[0])).toThrow();
+        expect(() => setUnavailable(0, 0, professors[0])).toThrow();
+
+        expect(getTimetableOf(classes[0]).getSubjectOn(0, 0)).toBe(subjects[0]);
+        expect(getTimetableOf(professors[0]).getSubjectOn(0, 0)).toBe(subjects[0]);
+    });
+
+    it("shouldn't throw when setting available on an assigned timeslot", () => {
+        setSubject(0, 0, subjects[0]);
+
+
+        setAvailable(0, 0, classes[0])
+        setAvailable(0, 0, professors[0])
+
+        expect(getTimetableOf(classes[0]).getSubjectOn(0, 0)).toBe(subjects[0]);
+        expect(getTimetableOf(professors[0]).getSubjectOn(0, 0)).toBe(subjects[0]);
+    });
+
+    it("should throw when removing a subject from an unassigned timeslot", () => {
+        expect(() => removeSubject(0, 0, subjects[0])).toThrow();
+    });
+
+    it("shouldn't throw when setting available on an unassigned timeslot", () => {
+        expect(() => setAvailable(0, 0, classes[0])).not.toThrow();
+    });
+
+    it("shouldn't throw when setting unavailable on an unavailable timeslot", () => {
+        setUnavailable(0, 0, classes[0]);
+
+        expect(() => setUnavailable(0, 0, classes[0])).not.toThrow();
+    });
+
+
+    test.each([-123, -1, 7, 2.1, 1234356])('throws on invalid dayOfWeek %p', (value) => {
+        expect(() => setSubject(value, 0, subjects[0])).toThrow();
+    });
+
+    test.each([-123, -1, 2.1, 13, 1234356])('throws on invalid timeOfDay %p', (value) => {
+        expect(() => setSubject(0, value, subjects[0])).toThrow();
+    });
+
+
+
 });
