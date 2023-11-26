f10acb6960790805ade014335b65e2eedb8805a5
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-11-26 12:36:38 +0100

    fixed editing while sorting bug
    and fixed outdated method call signature

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,13 +10,19 @@
 	import { Container, Styles } from "sveltestrap";
 	import { goto } from "$app/navigation";
 	import ThemeSwitcher from "$lib/components/ThemeSwitcher.svelte";
-    import { Navbar, NavbarBrand } from "sveltestrap";
+	import { Navbar, NavbarBrand } from "sveltestrap";
 
 	onMount(() => {
 		const generate_file = () =>
 			localStorage.setItem(
 				"data.tdf",
-				generateCookieFile($allProfessors, $allClassrooms, $allSubjects)
+				generateCookieFile(
+					$allProfessors,
+					$allClassrooms,
+					$allSubjects,
+					[],
+					[],
+				),
 			);
 
 		allProfessors.subscribe(generate_file);
@@ -25,48 +31,53 @@
 	});
 </script>
 
-
 <Styles theme={$theme} />
 
 <Navbar expand="md">
 	<div class="left-group">
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-		<p id="title" class="zoom-hover" on:click={() => {goto("/")}} >SCHOOL TIMETABLE</p>
-		
+		<p
+			id="title"
+			class="zoom-hover"
+			on:click={() => {
+				goto("/");
+			}}
+		>
+			SCHOOL TIMETABLE
+		</p>
+
 		<div class="vl"></div>
 		<div class="zoom-hover">
-			<NavbarBrand style="margin-left: 20px;" href="/data-input-form"> Input Form </NavbarBrand>
+			<NavbarBrand style="margin-left: 20px;" href="/data-input-form">
+				Input Form
+			</NavbarBrand>
 		</div>
-		
 	</div>
 
 	<ThemeSwitcher />
-
 </Navbar>
 <Container xxl class="bg-body rounded shadow mt-2">
 	<slot />
 </Container>
 
-
 <style>
-    p {
-        font-size: 23px;
-        font-weight: bold;
-        cursor: pointer;
-        margin: 0 0 0 30px;
-        padding: 0;
-    }
+	p {
+		font-size: 23px;
+		font-weight: bold;
+		cursor: pointer;
+		margin: 0 0 0 30px;
+		padding: 0;
+	}
 
-    .left-group {
-        display: flex;
-        align-items: center;
-    }
+	.left-group {
+		display: flex;
+		align-items: center;
+	}
 
-    .vl {
-        border-left: 1px solid;
-        height: 30px;
-        margin-left: 20px;
-    }
-        
+	.vl {
+		border-left: 1px solid;
+		height: 30px;
+		margin-left: 20px;
+	}
 </style>
