3088a979a8c3e65fd1c6bbde280b38843e389895
Gianmarco La Marca <132087463+GLAwasTaken@users.noreply.github.com>
2023-12-01 17:33:52 +0100

    Fixed mock tests

--- src/services/__mocks__/ClassService.ts
+++ src/services/__mocks__/ClassService.ts
@@ -1,13 +1,15 @@
-import type { Class } from "$lib/model";
+import { SchoolClass } from "../../model/school-class/school-class";
 
 export class ClassService {
-    getClasses(num: number): Class[] {
-        let res = [];
+    getClasses(num: number): SchoolClass[] {
+        let res: SchoolClass[] = [];
         for (let i = 0; i<num; i++) {
-            res.push({
-                id: i,
-                name: "className_"+i
-            });
+            let schoolClass: SchoolClass = SchoolClass.of(
+                (i+1).toString(),
+                Math.floor(Math.random()*i)+1,
+                String.fromCharCode("A".charCodeAt(0)+i)
+            )
+            res.push(schoolClass);
         }
         return res;
     }
