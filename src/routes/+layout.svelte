e9fc6f03532893c8f722245c010fc8b10cd78870
Gianmarco La Marca <132087463+GLAwasTaken@users.noreply.github.com>
2023-12-16 19:19:45 +0100

    Added home page and updated navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -57,19 +57,23 @@
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 
-		<p
-			id="title"
-			class="zoom-hover"
-			on:click={() => {
-				goto("/drag-and-drop");
-			}}
-		>
-			SCHOOL TIMETABLE
-		</p>
-
+		<div class="zoom-hover mx-2">
+			<NavbarBrand href="/">
+				<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+					<path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
+				</svg>
+			</NavbarBrand>
+		</div>
+		<div class="zoom-hover mx-2">
+			<NavbarBrand href="/drag-and-drop">Timetable</NavbarBrand>
+		</div>
 		<div class="vl mx-4"></div>
 		<div class="zoom-hover mx-2">
 			<NavbarBrand href="/data-input-form">Input Form</NavbarBrand>
+		</div>
+		<div class="vl mx-4"></div>
+		<div class="zoom-hover mx-2">
+			<NavbarBrand href="/downloadUploadData">Download-Upload Data</NavbarBrand>
 		</div>
 		<div class="zoom-hover mx-2">
 			<ExportTimetableForm></ExportTimetableForm>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,65 @@
+<div class="container">
+    <div class="title">
+        <h2>Welcome to SCHOOL TIMETABLE!</h2>
+    </div>
+    <div class="intro">
+        <p>
+            "School Timetable" is an editor that supports the user in the creation and management of timetables for schools.<br>
+            It is based on three sections:
+        </p>
+        <ul>
+            <li>
+                <b>Input Form</b><br>
+            </li>
+            <li>
+                <b>School Timetable</b>
+            </li>
+            <li>
+                <b>Download-Upload Data</b>
+            </li>
+        </ul>
+    </div>
+    <div class="description">
+        <p>
+            In the '<a href="/data-input-form"><b>Input Form</b></a>' section you can insert the datas to be used in your timetable.<br>
+            There you can either import datas from your local filesystem with the "import" button or
+            insert them manually with the forms.
+        </p>
+        <p>
+            In the '<a href="/drag-and-drop"><b>School Timetable</b></a>' section you can use the datas from the previous section
+            to create the timetable, from both the view of the professor or the class.<br>
+            You have two ways of creating the timetable:
+        </p>
+        <ol>
+            <li>by <em>manually</em> drag-and-dropping items from the sidebar to the table<br></li>
+            <li>by making the AI <em>automatically</em> generate the timetable from a set of given constraint<br></li>
+        </ol>
+        <p>
+            In the '<a href="/downloadUploadData"><b>Download-Upload Data</b></a>' section you can choose to store the current
+            session in your filesystem or to upload an existing one to start your work from where you left!
+        </p>
+        <p>
+            After you've completed the generation of the timetable, you can choose to export the result in PDF or Excel.
+        </p>
+    </div>
+    <p>We as the development team hope that you will find this editor useful. Enjoy!</p>
+</div>
+
+<style>
+    p {
+        font-size: 1.1em;
+    }
+
+    .container {
+        display: grid;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+
+    .title {
+        display: flex;
+        justify-content: center;
+        margin: 40px;
+    }
+</style>
