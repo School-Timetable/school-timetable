1f6d3d51449f336fb1ee09fc723af4a0cb793633
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2023-12-16 11:44:35 +0100

    untraked and removed .svelte-kit files

--- .svelte-kit/adapter-node/nodes/5.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 5;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/drag-and-drop/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/5.6cb00161.js","_app/immutable/chunks/index.6f3fdfc3.js","_app/immutable/chunks/Offcanvas.svelte_svelte_type_style_lang.5b4fd23b.js","_app/immutable/chunks/index.1a787e9c.js"];
-export const stylesheets = ["_app/immutable/assets/5.d699897e.css","_app/immutable/assets/Offcanvas.60614438.css"];
-export const fonts = [];
--- .svelte-kit/generated/client-optimized/nodes/3.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/data-input-form/+page.svelte";
