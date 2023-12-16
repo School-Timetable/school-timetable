2262d448647b71291fbda420e35830c10d7ad8c6
ilveron <alessandromonetti@outlook.it>
2023-12-16 11:19:35 +0100

    Add error dealt with in csv import alerts

--- src/model/school-class/year.ts
+++ src/model/school-class/year.ts
@@ -1,7 +1,12 @@
 import { z } from 'zod';
 
-export const valueSchema = z.number()
-    .int()
+export const valueSchema = z.number({errorMap: (issue) => {
+        switch (issue.code) {
+            default:
+                return { message: 'The year is not a number' };
+        }
+}})
+    .int("The year given is not an integer number")
     .min(1, { message: "The year given is too small (min 1)" })
     .max(5, { message: "The year given is too big (max 5)" })
 
